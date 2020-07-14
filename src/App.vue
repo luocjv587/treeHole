ecb69f575806e4f1db4697f6d35c430c9ab9111f
Luo <547605677@qq.com>
2020-07-14 18:07:24 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+<!--    <img src="./assets/logo.png">-->
+<!--    <router-view/>-->
+    <div id="header" class="full-rotate"><h1>This website will self destruct</h1> <h3>(in 86,153 seconds)</h3></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style src="./assets/css/main.css"></style>
+
+
