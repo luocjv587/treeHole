eedc3b83a5bc52d4569d27c9278c6ee5a262177c
Luo <547605677@qq.com>
2020-07-21 17:23:22 +0800

    增加展示喜爱数 和 获取数

--- config/dev.env.js
+++ config/dev.env.js
@@ -4,5 +4,5 @@ const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  VUE_APP_BACK_URL: '"http://127.0.0.1:9501"',
+  VUE_APP_BACK_URL: '"http://39.104.182.98:9502"',
 })
